1c8b41deb43a863413490f225b8eef5958b4e54b
psenakdotsk <jan@psenak.sk>
2025-11-04 19:30:18 +0100

    more stuff in svelte rewrite

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <br><hr>
 <p class="p-4">
   (c) 2025 Dvarwen Studios (site by 
-  <a class="relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-current after:transition-all hover:after:w-full" href="https://jan.psenak.sk">Ján Pšenák</a>)
+  <a class="relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:bg-current after:transition-all hover:after:w-0" href="https://jan.psenak.sk">Ján Pšenák</a>)
 </p>
