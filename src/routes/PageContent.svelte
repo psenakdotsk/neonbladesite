31507d9feaa4cf3fdb4a737b499bced17cf27945
psenakdotsk <psenakdotsk@noreply.codeberg.org>
2025-11-12 13:31:04 +0100

    Update src/routes/PageContent.svelte

--- src/routes/PageContent.svelte
+++ src/routes/PageContent.svelte
@@ -10,7 +10,7 @@ Take back the city — one bullet at a time.</p>
 <h1>"Assimilation is not optional."</h1>
 <p>Watch the trailer</p>
 <br>
-<div class="flex flex-col items-center"><iframe width="560" height="315" src="https://www.youtube.com/embed/c5v39cTGVok?si=oqlAjJOz5HWhlsNl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
+<div class="flex flex-col items-center"><iframe width="560" height="315" src="https://www.youtube.com/embed/8S2MEfBy2mU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
 
 <h1>Screenshots</h1>
 <div class="flex w-screen" style="overflow-x: auto;">
