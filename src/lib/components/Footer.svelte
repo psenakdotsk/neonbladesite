4b894e00ad799899b773850ecb0f15954143f011
psenakdotsk <jan@psenak.sk>
2025-11-17 15:04:08 +0100

    add backlink to dwarven site

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <br><hr>
 <p class="p-4">
-  (c) 2025 Dvarwen Studios (site by 
+  (c) 2025 <a class="relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:bg-current after:transition-all hover:after:w-0 text-primary" href="https://dwarven.pages.dev">Dvarwen Studios</a> (site by 
   <a class="relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:bg-current after:transition-all hover:after:w-0 text-primary" href="https://jan.psenak.sk">Ján Pšenák</a>)
 </p>
